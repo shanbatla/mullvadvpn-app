22c9853b84a627e871c437f2522d97791375c2ab
Janito Vaqueiro Ferreira Filho <janito@mullvad.net>
2020-12-02 21:05:34 +0000

    Allow registering event listener messengers

--- android/src/main/kotlin/net/mullvad/mullvadvpn/service/ServiceHandler.kt
+++ android/src/main/kotlin/net/mullvad/mullvadvpn/service/ServiceHandler.kt
@@ -3,7 +3,16 @@ package net.mullvad.mullvadvpn.service
 import android.os.Handler
 import android.os.Looper
 import android.os.Message
+import android.os.Messenger
 
 class ServiceHandler(looper: Looper) : Handler(looper) {
-    override fun handleMessage(message: Message) {}
+    private val listeners = mutableListOf<Messenger>()
+
+    override fun handleMessage(message: Message) {
+        val request = Request.fromMessage(message)
+
+        when (request) {
+            is Request.RegisterListener -> listeners.add(request.listener)
+        }
+    }
 }
