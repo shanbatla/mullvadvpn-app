d94510acd6dd2358e2c361f1dc43f4db96c2700a
Janito Vaqueiro Ferreira Filho <janito@mullvad.net>
2020-12-04 21:58:18 +0000

    Register for service events in `ServiceConnection`

--- android/src/main/kotlin/net/mullvad/mullvadvpn/ui/ServiceConnection.kt
+++ android/src/main/kotlin/net/mullvad/mullvadvpn/ui/ServiceConnection.kt
@@ -1,17 +1,25 @@
 package net.mullvad.mullvadvpn.ui
 
+import android.os.Looper
+import android.os.Messenger
+import android.os.RemoteException
+import android.util.Log
 import net.mullvad.mullvadvpn.dataproxy.AppVersionInfoCache
 import net.mullvad.mullvadvpn.dataproxy.RelayListListener
+import net.mullvad.mullvadvpn.service.Request
 import net.mullvad.mullvadvpn.service.ServiceInstance
+import net.mullvad.mullvadvpn.ui.serviceconnection.EventDispatcher
 import net.mullvad.mullvadvpn.ui.serviceconnection.LocationInfoCache
 
 class ServiceConnection(private val service: ServiceInstance, val mainActivity: MainActivity) {
+    val dispatcher = EventDispatcher(Looper.getMainLooper())
+
     val daemon = service.daemon
     val accountCache = service.accountCache
     val connectionProxy = service.connectionProxy
     val customDns = service.customDns
     val keyStatusListener = service.keyStatusListener
-    val locationInfoCache = LocationInfoCache(service.messenger, service.locationInfoCache)
+    val locationInfoCache = LocationInfoCache(service.messenger, dispatcher)
     val settingsListener = service.settingsListener
     val splitTunneling = service.splitTunneling
 
@@ -21,11 +29,22 @@ class ServiceConnection(private val service: ServiceInstance, val mainActivity: 
     init {
         appVersionInfoCache.onCreate()
         connectionProxy.mainActivity = mainActivity
+        registerListener()
     }
 
     fun onDestroy() {
         appVersionInfoCache.onDestroy()
         relayListListener.onDestroy()
         connectionProxy.mainActivity = null
+    }
+
+    private fun registerListener() {
+        val request = Request.RegisterListener(Messenger(dispatcher))
+
+        try {
+            service.messenger.send(request.message)
+        } catch (exception: RemoteException) {
+            Log.e("mullvad", "Failed to register listener for service events", exception)
+        }
     }
 }
